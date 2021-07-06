24a053abdd690c5fb6db1e8d0c355964ea1fd075
Mubashir <mubashircheruvayoor@gmail.com>
2021-07-07 00:54:03 +0530

    Collect static and update model

--- myProject/homeApp/views.py
+++ myProject/homeApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Users
 # Create your views here.
+def index(request):
+    users = Users.objects.all()
+    index=1
+    for user in users:
+        first=''
+        if index % 3 == 1 :
+            first='first' 
+        else:
+            first=''
+        index+=1
+        setattr(user,'first',first)
+    return render(request,'index.html',{'Aluminies':users})
